785290edef33b82f18a494998d677d388353f7cb
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-08-23 13:58:04 +0100

    Add animation for moving and spawning

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const COLOURS = {
 
 const STYLES = {
   GRID_STROKE: '#ccc',
-  ENTITIES_FILL: [
+  ENTITIES: [
     '#c33',
     '#39c',
     '#fc3',
@@ -43,7 +43,7 @@ class App {
     };
     
     this.entities = {};
-    this.entityStyles = {};  //Visual style of entities. Derived randomly.
+    this.entityStyles = {};  //Visual style of entities. Derived when entities are spawned.
     
     this.initialiseCanvas();
     this.updateUI_consoleRun();
@@ -102,28 +102,43 @@ class App {
     }
     
     //If this is the very start of an event... do something?
-    if (this.currentTick === 0) {}
-    
-    //PAINT PAINT PAINT!
-    this.paint();
-    
-    //Take the next step
-    this.currentTick++;
-    if (this.currentTick >= App.TICKS_PER_EVENT) {
-      this.currentTick = 0;
-      
-      this.currentRoundEvent++;      
-      if (this.currentRoundEvent >= round.events.length) {
-        this.currentRoundEvent = 0;
-        
-        this.currentRound++;
+    if (this.currentTick === 0) {
+      const event = round.events[this.currentRoundEvent];
+      if (event) {
+        switch (event.type) {
+          case "spawn":
+            const entityId = event.entity;
+            this.registerEntityStyle(entityId);
+            
+            //Add the entity to the list of current entities.
+            this.entities[entityId] = {
+              id: entityId,
+              coord: {
+                x: event.at.x,
+                y: event.at.y,
+              },
+            };
+            break;
+        }
       }
     }
     
-    //this.currentRound++;
+    //PAINT PAINT PAINT!
+    this.paint(round, round.events[this.currentRoundEvent]);
+    
+    //Take the next step
+    this.currentTick++;
+    if (this.currentTick >= App.TICKS_PER_EVENT) {  //If we've done all the ticks for this event, go to the next event.
+      this.currentTick = 0;
+      this.currentRoundEvent++;
+      if (this.currentRoundEvent >= round.events.length) {  //If we've done all the events for this round, go to the next round.
+        this.currentRoundEvent = 0;
+        this.currentRound++;
+      }
+    }
   }
   
-  paint() {
+  paint(round, event) {
     const w = this.map.width * App.TILE_SIZE;
     const h = this.map.height * App.TILE_SIZE;
     this.c2d.clearRect(0, 0, w, h);
@@ -143,26 +158,85 @@ class App {
     
     //For each entity, draw the character.
     Object.values(this.entities).forEach(entity => {
-      //TODO: move this to the "spawn" event
-      this.registerEntityStyle(entity);
+      //If an entity will be animated later (e.g. in an event), don't draw them
+      //at this stage.
+      const willEntityBeAnimatedLater = event
+        && event.entity === entity.id
+        && (event.type === "spawn" || event.type === "move");
+      if (willEntityBeAnimatedLater) return;
       
       const midX = (entity.coord.x + 0.5) * App.TILE_SIZE;
       const midY = (entity.coord.y + 0.5) * App.TILE_SIZE;
-      const radius = App.TILE_SIZE / 2;
-      const entityStyle = (this.entityStyles[entity.id])
-        ? this.entityStyles[entity.id]
-        : COLOURS.MISSING;
       
-      this.c2d.beginPath();
-      this.c2d.arc(midX, midY, radius, 0, 2 * Math.PI);
-      this.c2d.fillStyle = entityStyle;
-      this.c2d.fill();
+      this.paintEntity(entity.id, midX, midY, "idle");
     });
+    
+    //If there's an event, animate it.
+    const tweenPercent = this.currentTick / App.TICKS_PER_EVENT;
+    if (event) {
+      let entityId = event.entity;
+      let midX = 0;
+      let midY = 0;
+      let radius = 1;
+      let entityStyle = COLOURS.MISSING;
+      
+      switch (event.type) {
+          
+        //Event: player spawns.
+        //Animation: expanding circle.
+        case "spawn":
+          midX = (event.at.x + 0.5) * App.TILE_SIZE;
+          midY = (event.at.y + 0.5) * App.TILE_SIZE;
+          radius = Math.max(App.TILE_SIZE / 2 * tweenPercent, 1);
+          entityStyle = (this.entityStyles[entityId])
+            ? this.entityStyles[entityId]
+            : COLOURS.MISSING;
+
+          this.c2d.beginPath();
+          this.c2d.arc(midX, midY, radius, 0, 2 * Math.PI);
+          this.c2d.strokeStyle = entityStyle;
+          this.c2d.stroke();
+          
+          break;
+        
+        case "move":
+          midX = ((event.from.x + (event.to.x - event.from.x) * tweenPercent)
+                 + 0.5) * App.TILE_SIZE;
+          midY = ((event.from.y + (event.to.y - event.from.y) * tweenPercent)
+                 + 0.5) * App.TILE_SIZE;          
+          this.paintEntity(entityId, midX, midY, "moving");
+          
+        /* "type": "move",
+          "entity": "0eee3f36-36d1-46e7-be9c-e74a458b4eb8",
+          "from": {
+            "x": 7,
+            "y": 2
+          },
+          "to": {
+            "x": 7,
+            "y": 1
+          }*/
+        
+        default:
+      }
+    }
   }
   
-  registerEntityStyle(entity) {
-    if (!this.entityStyles[entity.id]) {
-      this.entityStyles[entity.id] = STYLES.ENTITIES_FILL[Object.values(this.entityStyles).length];
+  paintEntity(entityId, midX, midY, action) {
+    const radius = App.TILE_SIZE / 2;
+    const entityStyle = (this.entityStyles[entityId])
+      ? this.entityStyles[entityId]
+      : COLOURS.MISSING;
+
+    this.c2d.beginPath();
+    this.c2d.arc(midX, midY, radius, 0, 2 * Math.PI);
+    this.c2d.fillStyle = entityStyle;
+    this.c2d.fill();
+  }
+  
+  registerEntityStyle(entityId) {
+    if (!this.entityStyles[entityId]) {
+      this.entityStyles[entityId] = STYLES.ENTITIES[Object.values(this.entityStyles).length];
     }
   }
   
@@ -199,8 +273,8 @@ class App {
  */
 //==============================================================================
 App.TILE_SIZE = 32;  //Each tile is 32x32 pixels
-App.TICKS_PER_SECOND = 2;
-App.TICKS_PER_EVENT = 2;
+App.TICKS_PER_SECOND = 60;
+App.TICKS_PER_EVENT = 30;
 //==============================================================================
 
 /*  Initialisations
